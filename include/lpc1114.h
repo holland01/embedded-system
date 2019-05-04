90360498ba37bce83a7baa0f72c7effe5a5bfb67
schutth <schutth@wwu.edu>
2019-05-03 21:44:02 -0700

    more cleanup

--- include/lpc1114.h
+++ include/lpc1114.h
@@ -217,4 +217,51 @@ extern unsigned ICER;
 extern unsigned IOCON_PIO0_8;
 extern unsigned IOCON_R_PIO0_11;
 
+/* Masks are used either to
+   a) ensure that unwanted data isn't set, or
+   b) to zero out specific bits */
+
+/*  */
+
+#define SYSCON_PDRUNCFG_SYSPLL_OFF (1 << 7)
+#define SYSCON_PDRUNCFG_ADC_OFF (1 << 4)
+
+#define SYSCON_SYSPLLCLK_IRC 0
+#define SYSCON_MAINCLKSEL_PLL 3
+
+#define SYSCON_SYSAHBCLKCTRL_CT16B0_ON (1 << 7)
+#define SYSCON_SYSAHBCLKCTRL_ADC_ON (1 << 13)
+#define SYSCON_SYSAHBCLKCTRL_IOCON_ON (1 << 16)
+
+#define ADC_CR_SEL_CLEAR_MASK (~0xFF)
+#define ADC_CR_SEL_CHANNEL_SHIFT(x) (1 << ((x))
+#define ADC_CR_SEL_SET_CHANNEL(x) ( (ADC.CR & ADC_CR_SEL_CLEAR_MASK) | ADC_CR_SEL_CHANNEL_SHIFT((x))) )
+
+#define ADC_CR_BURST_ON (1 << 16)
+#define ADC_CR_BURST_SET_OFF (ADC.CR & (~ADC_CR_BURST_ON))
+
+#define ADC_CR_CLKDIV_CLEAR_MASK 0xFFFF00FF
+#define ADC_CR_CLKDIV_SHIFT(x) ((x) << 11)
+#define ADC_CR_CLKDIV_SET_VALUE(x) ((ADC.CR & ADC_CR_CLKDIV_CLEAR_MASK) | ADC_CR_CLKDIV_SHIFT((x)))
+
+#define ADC_CR_CLKS_MASK ((1 << 17) | (1 << 18) | (1 << 19))
+#define ADC_CR_CLKS_SET_11_CLK_10_BIT ((ADC.CR & ADC_CR_CLKS_MASK) | ~ADC_CR_CLKS_MASK)
+
+#define ADC_CR_START_MASK ((1 << 24) | (1 << 25) | (1 << 26))
+
+#define ADC_CR_START_SET_NO_START (ADC.CR & (~ADC_CR_START_MASK))
+
+#define ADC_INTEN_ADINTEN_CHANNEL(x) (1 << (x))
+#define ADC_INTEN_ADGINTEN (1 << 8)
+
+#define ADC_INTEN_SET_ADGINTEN_ONLY ((ADC.INTEN & (~ADC_INTEN_ADINTEN_CHANNEL(0))) | ADC_INTEN_ADGINTEN)
+
+#define ISER_IRQ16_ENABLED (ISER | (1 << 16))
+#define ISER_IRQ24_ENABLED (ISER | (1 << 24))
+
+#define TMR16B0_MCR_ENABLE_MR1_I (TMR16B0.MCR | (1 << 3))
+#define TMR16B0_MCR_ENABLE_MR1_R (TMR16B0.MCR | (1 << 4))
+
+#define TMR16B0_PWMC_ENABLE_MR0 (TMR16B0.PWMC | (1 << 0))
+
 #endif // __LPC1114_H__
